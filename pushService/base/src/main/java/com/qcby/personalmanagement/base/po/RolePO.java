f68cffde8d9f51b98663e55325d23e60c1829af2
张吉祥 <2809994543@qq.com>
2023-07-10 12:46:35 +0800

    自动填充字段po类更改

--- pushService/base/src/main/java/com/qcby/personalmanagement/base/po/RolePO.java
+++ pushService/base/src/main/java/com/qcby/personalmanagement/base/po/RolePO.java
@@ -1,8 +1,6 @@
 package com.qcby.personalmanagement.base.po;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -47,26 +45,31 @@ public class RolePO implements Serializable {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     /**
      * 更新时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     /**
      * 创建者
      */
+    @TableField(fill = FieldFill.INSERT)
     private String createBy;
 
     /**
      * 更新者
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private String updateBy;
 
     /**
      * 删除标志（0未删除，1已删除）
      */
+    @TableLogic
     private Integer deleteFlag;
 
     /**
